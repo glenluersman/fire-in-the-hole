401e893e398c62ebc4ca7038e64232b8295c0615
Niraj Limbu <nlimbu@yahoo.com>
2022-04-02 11:54:01 -0400

    update client files

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import Nav from './components/Nav';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Nav from './components/Nav';
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import Login from './pages/Login';
@@ -11,14 +11,12 @@ function App() {
   return (
     <Router>
       <div>
-        {/* <Nav /> */}
-        
-          <Route exact path='/' component={Home} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/signup' component={Signup} />
-          <Route exact path='/orderHistory' component={OrderHistory} />
-          <Route exact path='/products/:id' component={Detail} />
-        
+        <Nav />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/signup' component={Signup} />
+        <Route exact path='/orderHistory' component={OrderHistory} />
+        <Route exact path='/products/:id' component={Detail} />
       </div>
     </Router>
   );
