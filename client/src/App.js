4bf3adb0e72ab1734a30e68ea126c7430a06bc1c
Niraj Limbu <nlimbu@yahoo.com>
2022-04-02 00:29:00 -0400

    add pages and component files

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+import Login from './pages/Login';
+import OrderHistory from './pages/OrderHistory';
+import Signup from './pages/Signup'
+
 
 function App() {
   return (
