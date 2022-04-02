edf5cd767c7a676aaa81c2bb3861d5b9399382a5
Niraj Limbu <nlimbu@yahoo.com>
2022-04-02 01:21:31 -0400

    create client side mutation

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,26 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Nav from './components/Nav';
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import Login from './pages/Login';
 import OrderHistory from './pages/OrderHistory';
-import Signup from './pages/Signup'
-
+import Signup from './pages/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/signup' component={Signup} />
+          <Route exact path='/orderHistory' component={OrderHistory} />
+          <Route exact path='/products/:id' component={Detail} />
+        </switch>
+      </div>
+    </Router>
   );
 }
 
