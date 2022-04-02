d8010a5d37f4afcc88c0d4ff7badddd20647f91e
glenluersman <glen.luersman@gmail.com>
2022-04-02 13:30:20 -0400

    Category model

--- server/models/index.js
+++ server/models/index.js
@@ -2,5 +2,6 @@ const User = require('./User');
 const Product = require('./Product');
 const Review = require('./Review');
 const Order = require('./Order');
+const Category = require('./Category')
 
-module.exports = { User, Product, Review, Order };
+module.exports = { User, Product, Review, Order, Category };
