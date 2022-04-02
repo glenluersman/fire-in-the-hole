f84be4784a6d1b937f5d0273b247cfac89ea812a
Niraj Limbu <nlimbu@yahoo.com>
2022-04-02 01:30:58 -0400

    create client queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,3 +1,42 @@
-import {gql} from 'graphql-tag';
+import { gql } from 'graphql-tag';
 
-export const 
+export const QUERY_USER = gql`
+  {
+    user {
+      firstName
+      lastName
+      orders {
+        _id
+        purchaseDate
+        products {
+          _id
+          productName
+          price
+          description
+          ingredients
+          image
+          quantity
+        }
+      }
+    }
+  }
+`;
+
+export const QUERY_PRODUCTS = gql`
+  query getProducts($products: ID) {
+    products(products: $products) {
+      _id
+      productName
+      price
+      description
+      ingredients
+      image
+      quantity
+      reviews {
+        _id
+        rating
+        reviewText
+      }
+    }
+  }
+`;
