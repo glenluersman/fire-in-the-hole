c957131c5d8766e0a727b77c17c7620e17f57be7
sakme <alan@akme.us>
2022-04-05 13:30:31 -0400

    resolver correction to search for context.username

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,7 +6,7 @@ const resolvers = {
     Query: {
         user: async (parent, args, context) => {
             if (context.user) {
-                const user = await User.findById(_id)
+                const user = await User.findById(context.user._id)
                     .populate('reviews')
                     .populate({
                         path: 'orders.products',
