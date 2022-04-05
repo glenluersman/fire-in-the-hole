2d17436781423a41ad359736c5fc1268326c71ec
Funky Dad <jacobmontgomeryallred@gmail.com>
2022-04-04 20:19:57 -0400

    Server path Working

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/hotsauce', {
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGODB_URI_DEV, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
