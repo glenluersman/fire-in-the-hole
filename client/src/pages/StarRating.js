c23063bb0b11dfa0317cfd66fdd22e333ce8b535
Niraj Limbu <nlimbu@yahoo.com>
2022-04-08 18:45:50 -0400

    update star rating file

--- client/src/pages/StarRating.js
+++ client/src/pages/StarRating.js
@@ -1,19 +1,16 @@
 import React, { useState } from 'react';
 import { FaStar } from 'react-icons/fa';
 import "../assets/StarRatings.css";
-import { QUERY_PRODUCTS } from '../utils/queries';
-import { useQuery } from '@apollo/client';
 
 const StarRating = () => {
   const [rating, setRating] = useState(null);
   const [hover, setHover] = useState(null);
-  const {ratedIndex} = useQuery(QUERY_PRODUCTS);
 
   return (
     <div>
       {[...Array(5)].map((star, i) => {
         const ratingValue = i + 1;
-        
+
         return (
           <label>
             <input
@@ -27,7 +24,7 @@ const StarRating = () => {
               color={ratingValue <= (hover || rating) ? '#ffc107' : '#e4e5e9'}
               size={20}
               onMouseEnter={() => setHover(ratingValue)}
-              onMouseLeave={() => {setHover(null)}}
+              onMouseLeave={() => setHover(null)}
             />
             
           </label>
