983fd7883efcde26007d316e79addf7e04af685f
glenluersman <glen.luersman@gmail.com>
2022-04-11 21:41:26 -0400

    changed width on mobile product card

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -35,7 +35,7 @@ function ProductItem(item) {
   }
 
   return (
-    <div className='card product width-31'>
+    <div className='card product width'>
       <Link to={`/products/${_id}`}>
         <img alt={name} src={`/images/${image}`} />
         <p>{name}</p>
